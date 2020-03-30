2cd2c52cebc0849533dbbe81dafd8c87cb8a5c6a
Stefan Andersson <stefan.andersson@aanc.se>
2020-03-30 11:29:27 +0200

    Initial commit

---
+++ Get-UserByName.ps1
@@ -0,0 +1,29 @@
+﻿function Get-UserByName
+ {
+     [CmdletBinding()]
+     param
+     (
+         [Parameter()]
+         [string[]]$searchString = $null,
+         [bool]$AccountEnabled = $True
+     )
+     if ($searchString -eq $null)
+     {
+        Write-Host "You must provide a searchstring for the search"
+     }
+     else 
+     {
+        Write-Host "Searching for students containing $StudenName"
+        $finalSearchString = "*" + $searchString + "*"
+        if ($AccountEnabled -eq $True) 
+        {
+            $users = Get-ADUser -Filter {Enabled -eq $AccountEnabled -and GivenName -like $finalSearchString -or SurName -like $finalSearchString} -Properties extensionAttribute1, extensionAttribute15, LastLogonDate, Created
+        }
+        Else
+        {
+            $users = Get-ADUser -Filter {GivenName -like $finalSearchString -or SurName -like $finalSearchString} -Properties extensionAttribute1, extensionAttribute15, LastLogonDate, Created
+        }
+        $users
+     }
+
+ }
---
+++ Get-UsersWithMissingGoogleAccount.ps1
@@ -0,0 +1,20 @@
+﻿function Get-UsersWithMissingGoogleAccount
+{
+    $users = Get-ADUser -SearchBase "OU=KV,OU=04.ELEVER,OU=Användare,DC=UTBILDNING,DC=LOCAL" -Properties extensionattribute1,created,Lastlogondate -Filter {Enabled -eq $True} |Where extensionattribute1 -eq $null
+    foreach ($user in $users)
+    {
+        # Collect information
+        $SamAccountName = $user.SamAccountName
+        $GivenName = $user.GivenName
+        $SurName = $user.SurName
+        $DisplayName = $SurName + ", " + $GivenName
+        
+        # Create new Powershell object for output
+        $newObject = New-Object -TypeName psobject
+        $newObject | Add-Member -MemberType NoteProperty -Name SamAccountName -Value $SamAccountName
+        $newObject | Add-Member -MemberType NoteProperty -Name DisplayName -Value $DisplayName
+        $newObject | Add-Member -MemberType NoteProperty -Name SurName -Value $SurName
+        $newObject | Add-Member -MemberType NoteProperty -Name GivenName -Value $GivenName
+        $newObject
+    }
+}
